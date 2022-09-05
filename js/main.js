d98c9d86f8a2afa5e909fc13572483874567a940
asd1495 <armando.salcedo95@gmail.com>
2022-09-05 14:10:42 -0500

    added the components on separate files, index includes some of them already working together. Added logos and icons.

---
+++ js/main.js
@@ -0,0 +1,92 @@
+//Variables
+const menuEmail = document.querySelector('.navbar-email');
+const desktopMenu = document.querySelector('.desktop-menu');
+const hamburgerIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const productDetail = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container')
+
+//Event Listeners
+menuEmail.addEventListener('click', toggleDesktopMenu);
+hamburgerIcon.addEventListener('click', toggleMobileMenu);
+shoppingCart.addEventListener('click', toggleOrders);
+
+//Functions
+function toggleDesktopMenu() {
+    desktopMenu.classList.toggle('inactive')
+    productDetail.classList.add('inactive');
+}
+
+function toggleMobileMenu() {
+    productDetail.classList.add('inactive')
+    mobileMenu.classList.toggle('inactive');
+}
+
+function toggleOrders() {
+    mobileMenu.classList.add('inactive')
+    productDetail.classList.toggle('inactive')
+    desktopMenu.classList.add('inactive');
+}
+
+//Products Array
+const productsList = [];
+productsList.push({
+    name: 'Bike',
+    price: 120,
+    image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+productsList.push({
+    name: 'Cámara',
+    price: 22220,
+    image: 'https://images.pexels.com/photos/2929411/pexels-photo-2929411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+});
+
+productsList.push({
+    name: 'Horse',
+    price: 1029829,
+    image: 'https://static.wikia.nocookie.net/memes-pedia/images/a/a3/FB_IMG_1596591789564.jpg/revision/latest?cb=20200911174317&path-prefix=es',
+});
+
+//Products function
+function renderProducts(arr) {
+    for (product of arr) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', product.image);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './visual/YardSale_Icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCart);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+renderProducts(productsList);
+
